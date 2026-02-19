9bffdcc3785896944b3a9d3ed59c65a6fef0bc2f
kavee890 <kaveeshasenadeera67@gmail.com>
2026-02-19 22:02:41 +0530

    Delete IManufacturerApiService.cs

--- IManufacturerApiService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DistributionAPI.Services
-{
-    public interface IManufacturerApiService
-    {
-        Task<DTOs.StockResponseDTO> CheckManufacturerStock(int blanketId, int quantity);
-    }
-}
